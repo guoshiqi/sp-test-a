16d3fb4e70b5b41d1dda4bf6e165049cdaecf13f
lei.duan <lei.duan@yingzt.com>
2017-02-07 17:33:40 +0800

    update refill username and pass

--- app/src/main/assets/jd.js
+++ app/src/main/assets/jd.js
@@ -1,7 +1,6 @@
 
 dSpider("jd", function(session,env,$){
 
-    var re = /sid=(.+)$/ig;
     var infokey = "infokey";
     var sid = "";
     var max_order_num = 30;
@@ -76,7 +75,6 @@ dSpider("jd", function(session,env,$){
                if( globalInfo.order_info.order_detail.length <=  max_order_num && d.orderList.length!==0 && (orders.order_detail.length === 0 || d.orderList[d.orderList.length-1].orderId !== orders.order_detail[orders.order_detail.length-1].orderId) ){
                    orders.order_detail = orders.order_detail.concat(d.orderList);
                    var task = [];
-                   var tempOrder = [];
                    if(globalInfo.order_info.order_detail.length < max_order_num){
                         if(d.orderList.length + globalInfo.order_info.order_detail.length > max_order_num){
                            d.orderList = d.orderList.slice(0, max_order_num -  globalInfo.order_info.order_detail.length);
@@ -213,6 +211,181 @@ dSpider("jd", function(session,env,$){
         saveInfo();
         logout();
     }
+
+
+    if (location.href.indexOf("https://plogin.m.jd.com/user/login.action?appid=100") != -1 ) {
+            var n = !0;
+            log("xxxxxxxx " + session.getLocal("username"));
+             $("#username").val(session.getLocal("username"));
+             $("#password").val(session.getLocal("password"));
+             $("#loginBtn").on("click",
+                    function() {
+                        return !! $(this).hasClass("btn-active") && void o()
+                    })
+
+            function refreshAuth() {
+                var t = "/cgi-bin/m/authcode?mod=login&v=" + Math.random();
+                $("#imgCode").attr("src", t)
+            }
+             function a() {
+                        var t = {};
+                        t.username = $("#username").val(),
+                        t.pwd = $("#password").val();
+                        var e = str_rsaString;
+                        setMaxDigits(131);
+                        var i = new RSAKeyPair("3", "10001", e, 1024),
+                        o = window.btoa(encryptedString(i, t.pwd, RSAAPP.PKCS1Padding, RSAAPP.RawEncoding));
+                        t.pwd = o,
+                        t.remember = n,
+                        t.s_token = str_kenString;
+                        try {
+                            t.dat = getDat(t.username, t.pwd)
+                        } catch(a) {}
+                        return _need_ck && (t.authcode = $("#code").val()),
+                        t
+                    }
+
+            function t(t) {
+                        $(".pop-msg").html(t.msg),
+                        $(".btn-continue").attr("href", t.url).html(t.btn),
+                        $(".pop-dialog").show()
+            }
+            function e() {
+                $(".txt-input").on("input",
+                function() {
+                    $(this).siblings("i").show(),
+                    i() ? $("#loginBtn").addClass("btn-active") : $("#loginBtn").removeClass("btn-active")
+                })
+            }
+            function i() {
+                var t = !0;
+                return $(".txt-input").each(function() {
+                            if (!$(this).val()) return t = !1,
+                            !1
+                        }),
+                        t
+                    }
+
+            function o() {
+                    $(".input-container").removeClass("input-error"),
+                    $("#loginBtn").addClass("btn-active-disable").html("登录中"),
+                    $(".notice").html(" ");
+
+                    var n = a();
+                    log("set local xxxx" + $("#username").val());
+                    session.setLocal("password", $("#password").val());
+                    session.setLocal("username", $("#username").val());
+                    log("xxxxxxxx ins get  " + session.getLocal("username"));
+
+
+                    n.risk_jd = "";
+                    try {
+                        n.risk_jd = getJdEid()
+                    } catch(i) {}
+                    var o = (new Date).getTime();
+                    $.ajax({
+                        url: "/cgi-bin/m/domlogin",
+                        type: "POST",
+                        data: n,
+                        dataType: "json",
+                        success: function(i) {
+                            $("#loginBtn").removeClass("btn-active-disable").html("登录");
+                            var a = (new Date).getTime() - o;
+                            if (window.pl_report({
+                                interfaceID: 393217,
+                                loginName: n.username,
+                                callTime: a,
+                                status: i.errcode
+                            }), 0 == i.errcode) i.hk_autologin ? $.ajax({
+                                url: i.hk_url,
+                                type: "get",
+                                dataType: "jsonp",
+                                timeout: 15e3,
+                                complete: function() {
+                                    setTimeout(function() {
+                                        location.href = i.succcb
+                                    },
+                                    200)
+                                }
+                            }) : setTimeout(function() {
+                                location.href = i.succcb
+                            },
+                            200);
+                            else switch (i.needauth ? ($("#loginBtn").removeClass("btn-active"), $("#input-code").show().find("input").addClass("txt-input"), _need_ck = !0, refreshAuth(), e()) : ($("#input-code").hide().find("input").removeClass("txt-input"), _need_ck = !1, e()), i.errcode) {
+                            case 6:
+                                $("#password").parent().addClass("input-error"),
+                                $(".notice").html(i.message);
+                                break;
+                            case 7:
+                                $("#username").parent().addClass("input-error"),
+                                $(".notice").html(i.message);
+                                break;
+                            case 257:
+                                $("#code").val("").parent().addClass("input-error"),
+                                $(".notice").html(i.message);
+                                break;
+                            case 128:
+                            case 129:
+                            case 130:
+                            case 131:
+                            case 132:
+                            case 133:
+                            case 134:
+                            case 135:
+                            case 136:
+                            case 137:
+                            case 138:
+                            case 139:
+                            case 140:
+                            case 141:
+                            case 142:
+                            case 143:
+                                t({
+                                    msg:
+                                    i.message,
+                                    btn: "确定",
+                                    url: i.succcb
+                                });
+                                break;
+                            case 100:
+                                t({
+                                    msg:
+                                    "您的账号存在安全风险，请前往电脑版京东验证账号，有问题请致电京东客服4006065500",
+                                    btn: "确定",
+                                    url: "tel:4006065500"
+                                });
+                                break;
+                            case 103:
+                                t({
+                                    msg:
+                                    i.message,
+                                    btn: "找回密码",
+                                    url: "https://passport.m.jd.com/findloginpassword/fillAccountName.action"
+                                });
+                                break;
+                            case 105:
+                                location.href = i.succcb;
+                                break;
+                            default:
+                                $(".notice").html(i.message)
+                            }
+                        },
+                        error: function() {
+                            $(".notice").html("服务器开小差，请稍后重试"),
+                            $("#loginBtn").removeClass("btn-active-disable").html("登录");
+                            var t = (new Date).getTime() - o;
+                            window.pl_report({
+                                interfaceID: 393217,
+                                loginName: n.username,
+                                callTime: t,
+                                status: "255"
+                            })
+                        }
+                    })
+                }
+
+    }
+
 
     function saveInfo(){
         session.set(infokey, globalInfo);
